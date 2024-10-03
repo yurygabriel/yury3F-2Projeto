89a3aea933d5765a381c67cd26b3c576e614d622
yurygabriel <110116044+yurygabriel@users.noreply.github.com>
2024-10-03 09:43:19 -0300

    Update informacoesGlobais.js

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,10 +3,16 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function visualizaDadosGlobais(){
 const res = await fetch(url);
 const dados = await res.json();
-console.log(res);
+const totalPessoasMundo =  (dados.total_Pessoas_Mundo)/1e9;
+const totalPessoasConectadas =  (dados.total_Pessoas_Conectadas)/1e9;
+const tempoMedio = dados.tempo_medio;
+console.log(dados);
 
-const paragrafo = document.createElement('p')
-paragrafo.classList.add('graficos-texto')
-const container = document.getElementById('graficos-container')
+const paragrafo = document.createElement('p');
+paragrafo.classList.add('graficos-texto');
+const container = document.getElementById('graficos-container');
+paragrafo.innerHTML  = `O  mundo  tem ${totalPessoasMundo} bilhões de habitantes, dos quais ${totalPessoasConectadas} 
+bilhões estão  conectadas em alguma rede social por um tempo médio de ${tempoMedio}`
+container.appendChild(paragrafo);
 }
-visualizaDadosGlobais();
+visualizaDadosGlobais()
