38bf2614d72c4daacff14e48bad8766d879de7c3
yurygabriel <110116044+yurygabriel@users.noreply.github.com>
2024-10-18 14:22:31 -0300

    Update QuantidadeDeUsuarios.js

--- graficos/QuantidadeDeUsuarios.js
+++ graficos/QuantidadeDeUsuarios.js
@@ -1,15 +1,29 @@
+import { getCSS } from "./comun.js";
 async function quantidadeDeUsuarios(){
+
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
-    const res =  await fetch(url);
+    const res = await fetch(url)
     const dados = await res.json();
     const nomeDasRedes = Object.keys(dados);
-    const quantidadeDeUsuarios = Object.values(dados);
-    const infos = [
-        {
+    const  quantidadeDeUsuarios = Object.values(dados);
 
-            x: nomeDasRedes,
-            y: quantidadedeusuarios,
-            type: 'bar'
-        }
-    ]
+    const infos = [
+{
+    x: nomeDasRedes,
+    y: quantidadeDeUsuarios,
+    type: 'bar',
+marker:{
+color:getCSS('--cor-secundaria')
+
 }
+
+}
+    ]
+
+const grafico = document.createElement('div');
+grafico.className = 'grafico';
+document.getElementById('graficos-container').appendChild(grafico);
+Plotly.newPlot(grafico,infos);
+}
+
+quantidadeDeUsuarios();
