cb56a4cc7c64e3587f09216a34703246efbba8bc
yurygabriel <110116044+yurygabriel@users.noreply.github.com>
2024-10-24 09:39:50 -0300

    Update comum.js

--- graficos/comum.js
+++ graficos/comum.js
@@ -1,6 +1,6 @@
 
 const getCSS = (variavel) => {
-    return getComputedStyle(document.body).getPropertyPriority(variavel)
+    return getComputedStyle(document.body).getPropertyValue(variavel)
 }
 
 export {
