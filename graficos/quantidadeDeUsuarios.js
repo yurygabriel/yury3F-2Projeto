8ea96255220adeed897e1c1963e090a5ddedebcf
yurygabriel <110116044+yurygabriel@users.noreply.github.com>
2024-11-07 09:41:13 -0300

    Update quantidadeDeUsuarios.js

--- graficos/quantidadeDeUsuarios.js
+++ graficos/quantidadeDeUsuarios.js
@@ -1,5 +1,5 @@
 
-import { getCSS } from "./comum.js";
+import { getCSS, tickfont } from "./comum.js";
 async function quantidadeDeUsuarios(){
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url);
@@ -17,18 +17,37 @@ async function quantidadeDeUsuarios(){
             }
         }
     ]
+
     const layout = {
-        plot_bgcolor: getCSS('--cor-de-fundo'),
-        paper_bgcolor: getCSS('--cor-de-fundo'),
-        title: {
-            text: 'Redes sociais com mais usuários no mundo',
-            x: 0, 
-            font:{
-                color: getCSS('--cor-primaria'),
-                family: getCSS('--font'),
-                size: 30
-            } 
+      plot_bgcolor: getCSS('--cor-de-fundo'),
+      paper_bgcolor: getCSS('--cor-de-fundo'),
+      title:{
+        text: 'Redes socias com mais usuários no mundo',
+        x: 0,
+        font:{
+          color: getCSS('--cor-primaria'),
+          family: getCSS('--font'),
+          size:30
         }
+      },
+      xaxis:{
+        tickfont: tickfont,
+        title: {
+          text:'Nome das redes sociais',
+          font:{
+            color: getCSS('--cor-secundaria')
+          }
+        }
+      },
+      yaxis: {
+        tickfont: tickfont,
+        title:{
+          text: 'Milhões de usuários ativos',
+          font:{
+            color: getCSS('--cor-secundaria')
+          }
+        }
+      }
     }
 
     const grafico = document.createElement('div');
